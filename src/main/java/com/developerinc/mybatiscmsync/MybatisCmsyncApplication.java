e094caa258e00c8aa45389d0d7cc9e6c9c4fbdc7
Timothy Smithers <tsmithers@Timothys-MacBook-Pro-3.local>
2024-08-14 09:32:18 -0400

    init project

---
+++ src/main/java/com/developerinc/mybatiscmsync/MybatisCmsyncApplication.java
@@ -0,0 +1,13 @@
+package com.developerinc.mybatiscmsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisCmsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisCmsyncApplication.class, args);
+    }
+
+}
