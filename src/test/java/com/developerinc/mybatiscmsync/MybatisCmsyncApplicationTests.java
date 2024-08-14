b99c0df1fabb6e1e3b30b7d925c23a3f312387fd
Timothy Smithers <tsmithers@timothys-macbook-pro-3.local>
2024-08-14 09:32:18 -0400

    init project

---
+++ src/test/java/com/developerinc/mybatiscmsync/MybatisCmsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developerinc.mybatiscmsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisCmsyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
